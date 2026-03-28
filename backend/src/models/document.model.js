cd71ba1f32a454c2684599cf09109e1dbdd1d988
Taranpreet Singh <taranpreet1911@gmail.com>
2026-03-28 19:24:30 +0530

    made changes to the models

---
+++ backend/src/models/document.model.js
@@ -0,0 +1,34 @@
+import mongoose, {Schema} from 'mongoose'
+
+const docSchema = new Scehma(
+    {
+        owner: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        fileURL: {
+            type: String,
+        },
+        originalName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        storedName: {
+            type: String
+        },
+        size: {
+            type: Number,
+            required: true
+        },
+        fileType: {
+            type: String,
+            required: true
+        }
+
+    },{
+        timestamps: true
+    }
+)
